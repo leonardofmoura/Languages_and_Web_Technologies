8ce470086dcbffce5da744009e42d963c2abdc52
leonardofmoura <up201706907@fe.up.pt>
2019-10-23 10:21:21 +0100

    Fixed intro news not showing

--- TP3 - PHP/exercise5/templates/draw_new_headers.php
+++ TP3 - PHP/exercise5/templates/draw_new_headers.php
@@ -1,8 +1,8 @@
 <?php function draw_new_headers($articles) {
     foreach ($articles as $article) { ?>
         <article>
-            <h1><a href="#"><? $article['title'] ?></a></h1>
-            <p><? $article['introduction'] ?></p>
+            <h1><a href="#"><?= $article['title'] ?></a></h1>
+            <p><?= $article['introduction'] ?></p>
         </article>
     <?php } 
 } ?>
