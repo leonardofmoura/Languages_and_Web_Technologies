26cae38ed31aaf128a5510c1f1ca4dd3b4224d51
leonardofmoura <up201706907@fe.up.pt>
2019-10-23 09:53:46 +0100

    fixed database connection

--- TP3 - PHP/exercise5/database/connection.php
+++ TP3 - PHP/exercise5/database/connection.php
@@ -1,5 +1,5 @@
 <?php 
-    $db = new PDO('sqlite:news.db');
+    $db = new PDO('sqlite:database/news.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 ?>
