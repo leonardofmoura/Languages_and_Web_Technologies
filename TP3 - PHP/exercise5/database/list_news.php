543f8f085324aa33e167a997a48b609b5447700a
leonardofmoura <up201706907@fe.up.pt>
2019-10-23 10:17:21 +0100

    Added templates and testing intro news

--- TP3 - PHP/exercise5/database/list_news.php
+++ TP3 - PHP/exercise5/database/list_news.php
@@ -1,16 +1,30 @@
 <?php
     include_once('database/connection.php');
 
-    function list_news() {
+    function get_news_intro() {
         global $db;
 
         $stmt = $db->prepare('SELECT * FROM news');
         $stmt->execute();
         $articles = $stmt->fetchAll();
     
-        foreach ($articles as $article) {
-            echo '<h1>' . $article['title'] . '</h1>';
-            echo '<p>' . $article['introduction'] . '</p>';
+        return $articles;
+    }
+
+    function list_complete_new() {
+        global $db;
+
+        $stmt = $db->prepare('SELECT news.*, users.*, COUNT(comments.id) AS comments
+        FROM news JOIN
+             users USING (username) LEFT JOIN
+             comments ON comments.news_id = news.id
+        GROUP BY news.id, users.username
+        ORDER BY published DESC');
+        $stmt->execute();
+        $articles = $stmt->fetchAll();
+
+        foreach($articles as $article) {
+            
         }
     }
 ?>
